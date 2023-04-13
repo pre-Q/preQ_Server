56e96ce441e672f29c620eecfcd031191c3d5e78
minjikim <inkmjj@sookmyung.ac.kr>
2023-04-13 18:53:41 +0900

    initial commit

---
+++ src/main/java/kr/co/preq/PreqApplication.java
@@ -0,0 +1,13 @@
+package kr.co.preq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreqApplication.class, args);
+	}
+
+}
