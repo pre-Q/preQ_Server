2c6c4e94a404f51afbc1aa5af00098a410fdd470
Seokyeong <purewater71@naver.com>
2023-05-02 11:15:07 +0900

    [Chore] ec2 배포 오류로 인한 테스트 코드 주석 처리

--- src/main/java/kr/co/preq/global/common/entity/BaseEntity.java
+++ src/main/java/kr/co/preq/global/common/entity/BaseEntity.java
@@ -24,7 +24,7 @@ public abstract class BaseEntity {
 	@Getter
 	private Long id;
 
-	@Column(name = "created_at", updatable = false, nullable = false)
+	@Column(name = "created_date", updatable = false, nullable = false)
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	@CreatedDate
 	@Getter
