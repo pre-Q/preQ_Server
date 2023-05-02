06f04e3f2e5674cc74948a64eabb66c4cd4c8b15
Seokyeong <purewater71@naver.com>
2023-05-02 11:34:20 +0900

    [Fix] 빌드오류 해결

--- src/main/java/kr/co/preq/domain/user/entity/Member.java
+++ src/main/java/kr/co/preq/domain/user/entity/Member.java
@@ -1,4 +1,36 @@
 package kr.co.preq.domain.user.entity;
 
-public class Member {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.util.Assert;
+
+import kr.co.preq.global.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Builder
+    Member(String name, String email) {
+        Assert.notNull(name, "name must not be null");
+        Assert.notNull(email, "email must not be null");
+
+        this.name = name;
+        this.email = email;
+    }
 }
+
