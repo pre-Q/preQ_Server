b83e3b13bb3b254090e4b081e460323f70fca978
minjikim <inkmjj@sookmyung.ac.kr>
2023-05-11 22:59:10 +0900

    [fix] 토큰만료시간 수정

--- src/main/java/kr/co/preq/global/common/util/jwt/TokenInfo.java
+++ src/main/java/kr/co/preq/global/common/util/jwt/TokenInfo.java
@@ -8,8 +8,6 @@ public class TokenInfo {
 	public static final String AUTHORIZATION_HEADER = "Authorization";
 	public static final String BEARER_TYPE = "Bearer";
 	public static final int START_TOKEN_LOCATION = 7;
-	// public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L;             // 30분
-	public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 60 * 24 * 365L; // 1년
-	// public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7L;  // 1주
-	public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 60 * 24 * 365L; // 1년
+	public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 30L;	// 1달
+	public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 60L;  // 2달
 }
