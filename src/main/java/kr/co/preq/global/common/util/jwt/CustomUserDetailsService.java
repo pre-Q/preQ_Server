65e664f6ebdb8006f1094cc93d6c3d113c0415ca
minjikim <inkmjj@sookmyung.ac.kr>
2023-05-19 01:36:43 +0900

    [feat] refresh API 구현

--- src/main/java/kr/co/preq/global/common/util/jwt/CustomUserDetailsService.java
+++ src/main/java/kr/co/preq/global/common/util/jwt/CustomUserDetailsService.java
@@ -1,8 +1,10 @@
 package kr.co.preq.global.common.util.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.co.preq.domain.member.repository.MemberRepository;
