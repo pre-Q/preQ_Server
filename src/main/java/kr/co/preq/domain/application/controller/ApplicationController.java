85782d38c6047ae9a7d61d513eb652c9885a7ec1
minjikim <inkmjj@sookmyung.ac.kr>
2023-09-10 23:02:30 +0900

    [Fix] update application title and memo 오류 수정
    - @PathVariable 수정
    - NotFoundException -> BadRequestException으로 수정

--- src/main/java/kr/co/preq/domain/application/controller/ApplicationController.java
+++ src/main/java/kr/co/preq/domain/application/controller/ApplicationController.java
@@ -9,6 +9,12 @@ import kr.co.preq.global.common.util.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -30,24 +36,23 @@ public class ApplicationController {
 
         return ApiResponse.success(SuccessCode.APPLICATION_CREATE_SUCCESS, applicationId);
     }
-
-
+    
     @GetMapping("/list")
     public ApiResponse<List<ApplicationListGetResponseDto>> getApplicationList() {
         List<ApplicationListGetResponseDto> applicationList = applicationService.getApplicationList();
-
+        
         return ApiResponse.success(SuccessCode.APPLICATION_LIST_GET_SUCCESS, applicationList);
     }
 
     @PatchMapping("/{applicationId}/title")
-    public ApiResponse<Object> updateApplicationTitle(@RequestParam Long applicationId, @RequestBody ApplicationTitleUpdateRequestDto requestDto) {
+    public ApiResponse<Object> updateApplicationTitle(@PathVariable Long applicationId, @RequestBody ApplicationTitleUpdateRequestDto requestDto) {
         applicationService.updateApplicationTitle(applicationId, requestDto);
         
         return ApiResponse.success(SuccessCode.APPLICATION_TITLE_UPDATE_SUCCESS);
     }
 
     @PatchMapping("/{applicationId}/memo")
-    public ApiResponse<Object> updateApplicationMemo(@RequestParam Long applicationId, @RequestBody ApplicationMemoUpdateRequestDto requestDto) {
+    public ApiResponse<Object> updateApplicationMemo(@PathVariable Long applicationId, @RequestBody ApplicationMemoUpdateRequestDto requestDto) {
         applicationService.updateApplicationMemo(applicationId, requestDto);
         
         return ApiResponse.success(SuccessCode.APPLICATION_MEMO_UPDATE_SUCCESS);
