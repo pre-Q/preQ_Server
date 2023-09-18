697343223088ef9aa75b79d6d7fe52120bf090b4
minjikim <inkmjj@sookmyung.ac.kr>
2023-09-18 21:01:44 +0900

    [add] application child entity에 parentId 추가

--- src/main/java/kr/co/preq/domain/applicationChild/dto/request/ApplicationChildRequestDto.java
+++ src/main/java/kr/co/preq/domain/applicationChild/dto/request/ApplicationChildRequestDto.java
@@ -14,4 +14,7 @@ public class ApplicationChildRequestDto {
 
 	@NotNull(message = "답변이 없습니다.")
 	private String answer;
+
+	@NotNull(message = "상위 문항 Id가 없습니다. 상위 문항이 없는 경우 0을 보내주십시오.")
+	private Long parentId;
 }
